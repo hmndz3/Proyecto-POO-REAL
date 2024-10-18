863ec126f83e5937e18fa887674c2261abe878df
Oliwis338 <olivercifuentes338@gmail.com>
2024-10-17 21:45:45 -0600

    Atributos agregados a clase Horario.

--- horario.java
+++ horario.java
@@ -1,4 +1,49 @@
 public class Horario 
 {
+    private int identificador;          
+    private String correousuario;       
+    private int identificadorclase;     
+    private String fecha;              
+    private String hora;               
+
+    public Horario(int identificador, String correousuario, int identificadorclase, String fecha, String hora) {
+        this.identificador = identificador;
+        this.correousuario = correousuario;
+        this.identificadorclase = identificadorclase;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
     
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getCorreousuario() {
+        return correousuario;
+    }
+
+    public int getIdentificadorclase() {
+        return identificadorclase;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Horario{" +
+               "identificador=" + identificador +
+               ", correousuario='" + correousuario + '\'' +
+               ", identificadorclase=" + identificadorclase +
+               ", fecha='" + fecha + '\'' +
+               ", hora='" + hora + '\'' +
+               '}';
+    }
 }
