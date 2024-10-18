fd9bb5c8ccca75e5d2cfb24271895e1f78d0e393
Harry Mendez <harrymndz3@gmail.com>
2024-10-17 21:34:44 -0600

    Clase 'Clase' programada con sus atributos (setters, getters y constructor).

--- clase.java
+++ clase.java
@@ -1,4 +1,54 @@
 public class Clase 
 {
-    
+    private int identificador;     
+    private String nombre;          
+    private String codigoclase;     
+    private String profesor;        
+    private String horario;         
+    private String seccion;        
+
+    public Clase(int identificador, String nombre, String codigoclase, String profesor, String horario, String seccion) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.codigoclase = codigoclase;
+        this.profesor = profesor;
+        this.horario = horario;
+        this.seccion = seccion;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigoclase() {
+        return codigoclase;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase{" +
+               "identificador=" + identificador +
+               ", nombre='" + nombre + '\'' +
+               ", codigoclase='" + codigoclase + '\'' +
+               ", profesor='" + profesor + '\'' +
+               ", horario='" + horario + '\'' +
+               ", seccion='" + seccion + '\'' +
+               '}';
+    }
 }
