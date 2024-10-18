b2243fa7cbd4bfa092758921766ace6c21d31391
Sandoval <gtkaiser29@gmail.com>
2024-10-17 22:24:14 -0600

    Se agrego los primeros elementos de la clase sistema.

--- sistema.java
+++ sistema.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Sistema 
 {
-    
+    private ArrayList<Usuario> listausuarios;    
+    private ArrayList<Clase> listaclases;        
+
+    public Sistema() {
+        this.listausuarios = new ArrayList<>();
+        this.listaclases = new ArrayList<>();
+    }
 }
