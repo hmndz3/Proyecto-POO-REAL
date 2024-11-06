2607c0f4601ab306286d732639325291076c437f
Sandoval <gtkaiser29@gmail.com>
2024-11-05 19:27:53 -0600

    Implementación de banderas a los menús
    Se eliminaron los while true y el if(logged in) de main.java

--- main.java
+++ main.java
@@ -11,7 +11,9 @@ public class Main {
         sistema.agregarClase(new Clase(2, "FISICA l", "1FF20164020242", "ZAIDY URRITIA", "MARTES 10:00-12:00", "Sección 90"));
         sistema.agregarClase(new Clase(3, "CIUDADANIA GLOBAL", "1MM20026020242", "ANDREA CHAVEZ", "MIERCOLES 10:00-12:00", "Sección 30"));
 
-        while (true) {
+        boolean bandera1 = true;
+
+        while (bandera1) {
             System.out.println("\n--- Menú Inicial ---");
             System.out.println("1. Register");
             System.out.println("2. Log In");
@@ -52,7 +54,7 @@ public class Main {
 
                 case 3:
                     System.out.println("Saliendo del sistema...");
-                    System.exit(0);
+                    bandera1 = false;
                     break;
 
                 default:
@@ -60,67 +62,63 @@ public class Main {
                     break;
             }
 
-            if (loggedIn) {
-                while (true) {
-                    System.out.println("\n--- Menú de Opciones ---");
-                    System.out.println("1. MOSTRAR CLASES DISPONIBLES.");
-                    System.out.println("2. Inscribirse en Clase");
-                    System.out.println("3. Mostrar Horario de clases Inscritas.");
-                    System.out.println("4. Desasignar Clase");
-                    System.out.println("5. En Desarrollo");
-                    System.out.println("6. En Desarrollo");
-                    System.out.println("7. Cambiar datos");
-                    System.out.println("8. Cerrar sesión");
-                    System.out.print("Selecciona una opción: ");
-                    int opcionMenu2 = scanner.nextInt();
-                    scanner.nextLine(); 
+            boolean bandera2 = loggedIn;
 
-                    switch (opcionMenu2) {
-                        case 1:
-                            sistema.mostrarClasesDisponibles();
-                            break;
-                        case 2:
-                            System.out.print("Ingrese el código de la clase que desea inscribir: ");
-                            String codigoClase = scanner.nextLine();
-                            sistema.inscribirClase(correoLogin, codigoClase);
-                            break;
-                        case 3:
-                            sistema.mostrarHorarioUsuario(correoLogin);
-                            break;
-                        case 4:
-                            System.out.print("Ingrese el código de la clase que desea desasignar: ");
-                            String codigoClaseADesasignar = scanner.nextLine();
-                            sistema.desasignarClase(correoLogin, codigoClaseADesasignar);
-                            break;
-                        case 5:
-                        case 6:
-                            System.out.println("Esta funcionalidad aún está en desarrollo.");
-                            break;
+            while (bandera2) {
+                System.out.println("\n--- Menú de Opciones ---");
+                System.out.println("1. MOSTRAR CLASES DISPONIBLES.");
+                System.out.println("2. Inscribirse en Clase");
+                System.out.println("3. Mostrar Horario de clases Inscritas.");
+                System.out.println("4. Desasignar Clase");
+                System.out.println("5. En Desarrollo");
+                System.out.println("6. En Desarrollo");
+                System.out.println("7. Cambiar datos");
+                System.out.println("8. Cerrar sesión");
+                System.out.print("Selecciona una opción: ");
+                int opcionMenu2 = scanner.nextInt();
+                scanner.nextLine(); 
 
-                        case 7:
-                            System.out.println("Cambiar datos del usuario");
-                            System.out.print("Nuevo nombre: ");
-                            String nuevoNombre = scanner.nextLine();
-                            System.out.print("Nueva contraseña: ");
-                            String nuevaContrasena = scanner.nextLine();
+                switch (opcionMenu2) {
+                    case 1:
+                        sistema.mostrarClasesDisponibles();
+                        break;
+                    case 2:
+                        System.out.print("Ingrese el código de la clase que desea inscribir: ");
+                        String codigoClase = scanner.nextLine();
+                        sistema.inscribirClase(correoLogin, codigoClase);
+                        break;
+                    case 3:
+                        sistema.mostrarHorarioUsuario(correoLogin);
+                        break;
+                    case 4:
+                        System.out.print("Ingrese el código de la clase que desea desasignar: ");
+                        String codigoClaseADesasignar = scanner.nextLine();
+                        sistema.desasignarClase(correoLogin, codigoClaseADesasignar);
+                        break;
+                    case 5:
+                    case 6:
+                        System.out.println("Esta funcionalidad aún está en desarrollo.");
+                        break;
 
-                            Usuario.cambiarDatos(correoLogin, nuevoNombre, nuevaContrasena);
-                            System.out.println("Datos actualizados.");
-                            break;
+                    case 7:
+                        System.out.println("Cambiar datos del usuario");
+                        System.out.print("Nuevo nombre: ");
+                        String nuevoNombre = scanner.nextLine();
+                        System.out.print("Nueva contraseña: ");
+                        String nuevaContrasena = scanner.nextLine();
 
-                        case 8:
-                            System.out.println("Cerrando sesión...");
-                            loggedIn = false;
-                            break;
+                        Usuario.cambiarDatos(correoLogin, nuevoNombre, nuevaContrasena);
+                        System.out.println("Datos actualizados.");
+                        break;
 
-                        default:
-                            System.out.println("Opción no válida.");
-                            break;
-                    }
+                    case 8:
+                        System.out.println("Cerrando sesión...");
+                        loggedIn = false;
+                        bandera2 = false;
+                        break;
 
-                    if (!loggedIn) {
-                        break; 
-                    }
+                    default:
+                        System.out.println("Opción no válida.");
                 }
             }
         }
